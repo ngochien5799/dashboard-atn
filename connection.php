cd868b2f43a975a5c6ec38871ad4a8c9a640ac70
ngochien5799 <54261297+ngochien5799@users.noreply.github.com>
2019-08-19 05:24:54 -0500

    * Update db connection

--- connection.php
+++ connection.php
@@ -5,7 +5,7 @@ class DB
     public static function getInstance() {
       if (!isset(self::$instance)) {
         try {
-          self::$instance = new PDO('pgsql:host=ec2-107-21-216-112.compute-1.amazonaws.com;port=5432;dbname=d396l04psi4ibi', 'kbvppscldudvyk', '6b64aa00495e1869a781f324a4737159780da354a3cf8e8da84f9d84d44822a3');
+          self::$instance = new PDO('pgsql:host=ec2-23-21-91-183.compute-1.amazonaws.com;port=5432;dbname=d2v77k5qgq18kr', 'uunpzfggvhxbiu', '37cb35d0943314f0209129239277b2899225c0cc31f7f00f7f27f4e361426c05');
         } catch (PDOException $ex) {
           die($ex->getMessage());
         }
